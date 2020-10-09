9207162f941be3a68fe6aaca24ba8edb8c923f4d
MiThikeNon <mithikenonnonlay@gmail.com>
2020-10-09 21:22:46 +0630

    Routing Assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InformationComponent } from './information/information.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/Login', pathMatch: 'full'},
+    {path: 'Login', component : LoginComponent},
+    {path: 'registration', component : RegistrationComponent},
+    {path: 'dashboard', component : DashboardComponent},
+    {path: 'information', component : InformationComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { RegistrationComponent ; DashboardComponent ; InformationComponent ; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InformationComponent } from './information/information.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+     DashboardComponent,
+    InformationComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
